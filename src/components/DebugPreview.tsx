ab0fb86f318ac50839845308e1b37f0bed73e92a
ChipiCocoa <186157389+ChipiCocoa@users.noreply.github.com>
2026-03-08 17:26:23 +0800

    feat(i18n): translate ExpChart, CropSelector, and DebugPreview

--- src/components/DebugPreview.tsx
+++ src/components/DebugPreview.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next'
 import type { OcrDebugImages } from '../lib/ocr-service'
 import './DebugPreview.css'
 
@@ -6,17 +7,18 @@ interface DebugPreviewProps {
 }
 
 export function DebugPreview({ images }: DebugPreviewProps) {
+  const { t } = useTranslation()
   return (
     <div className="debug-preview">
-      <h3>OCR Debug</h3>
+      <h3>{t('debug.title')}</h3>
       <div className="debug-images">
         <div className="debug-item">
-          <span className="debug-label">Cropped Raw</span>
-          <img src={images.raw} alt="Raw cropped frame" />
+          <span className="debug-label">{t('debug.croppedRaw')}</span>
+          <img src={images.raw} alt={t('debug.rawAlt')} />
         </div>
         <div className="debug-item">
-          <span className="debug-label">Processed (sent to OCR)</span>
-          <img src={images.processed} alt="Preprocessed for OCR" />
+          <span className="debug-label">{t('debug.processed')}</span>
+          <img src={images.processed} alt={t('debug.processedAlt')} />
         </div>
       </div>
     </div>
